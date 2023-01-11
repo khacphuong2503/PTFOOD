749c755f8cfd5de4183c7a15cf6b4a579f5a75c6
Khac Phuong <phuongnk0912@gmail.com>
2023-01-11 16:42:24 +0700

    final

---
+++ DeliveryApp/DeliveryApp/Helpers/AddCategoryData.cs
@@ -0,0 +1,82 @@
+﻿using DeliveryApp.ViewModels;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DeliveryApp.Helpers
+{
+   public class AddCategoryData
+    {
+        FirebaseClient client;
+      
+        public List<Category> Categories { get; set;}
+        public AddCategoryData()
+        {
+            client = new FirebaseClient("https://deliveryapp-5d4e6-default-rtdb.asia-southeast1.firebasedatabase.app/");
+            Categories = new List<Category>()
+            {
+                new Category(){
+                    CategoryID = 1,
+                    CategoryName = "Burger11",
+                    CategoryPoster = "MainBurge1r",
+                    ImageUrl = "book.png"
+                },
+                new Category(){
+                    CategoryID = 2,
+                    CategoryName = "Pizza",
+                    CategoryPoster = "MainPizza",
+                    ImageUrl = "book.png"
+                },
+                new Category(){
+                    CategoryID = 3,
+                    CategoryName = "Desserts",
+                    CategoryPoster = "MainDessert.png",
+                    ImageUrl = "Dessert.png"
+                },
+                new Category(){
+                    CategoryID = 4,
+                    CategoryName = "Veg Burger",
+                    CategoryPoster = "MainBurger",
+                    ImageUrl = "Burger.png"
+                },
+                new Category(){
+                    CategoryID = 5,
+                    CategoryName = "Veg Pizza",
+                    CategoryPoster = "MainPizza.png",
+                    ImageUrl = "Pizza.png"
+                },
+                new Category(){
+                    CategoryID = 6,
+                    CategoryName = "Cakes",
+                    CategoryPoster = "MainDessert.png",
+                    ImageUrl = "Dessert.png"
+                }
+            };
+        }
+
+        public async Task AddCategoriesAsync()
+        {
+            try
+            {
+                foreach (var category in Categories)
+                {
+                    await client.Child("Categories").PostAsync(new Category()
+                    {
+                        CategoryID = category.CategoryID,
+                        CategoryName = category.CategoryName,
+                        CategoryPoster = category.CategoryPoster,
+                        ImageUrl = category.ImageUrl
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Lỗi", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ DeliveryApp/DeliveryApp/Services/CartItemService.cs
@@ -0,0 +1,29 @@
+﻿using DeliveryApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using DeliveryApp.Helpers;
+namespace DeliveryApp.Services
+{
+    public class CartItemService
+    {
+        public int  GetUserCartCount()
+        {
+            var t = new CreateCartTable();
+            if(!t.CreateTable()) return 0;
+            var cn = DependencyService.Get<ISQLite>().GetConnection();
+            var count = cn.Table<CartItem>().Count();
+            cn.Close();
+            return count;
+        }
+
+        public void RemoveItemsFromCart()
+        {
+            var cn = DependencyService.Get<ISQLite>().GetConnection();
+            cn.DeleteAll<CartItem>();
+            cn.Commit();
+            cn.Close();
+        }
+    }
+}
